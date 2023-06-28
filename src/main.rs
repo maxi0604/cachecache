51a9383322f603018a1d8a14dcc58b5bb87ffbcf
Maximilian R <maxi.rostock@outlook.de>
2023-06-28 20:25:14 +0200

    Add error types

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,43 @@
-use std::io::{Error, ErrorKind};
-use std::{env, fs};
+use std::str::FromStr;
+use std::error::Error;
+use std::{env, fs, fmt};
 
-#[derive(Clone, Copy)]
+#[derive(Clone, Copy, Debug)]
 enum Strategy {
     LRU,
     LFU,
 }
-#[derive(Clone)]
+
+impl FromStr for Strategy {
+    type Err = ParseStrategyError;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "LFU" => Ok(Strategy::LFU),
+            "LRU" => Ok(Strategy::LRU),
+            _ => Err(ParseStrategyError)
+        }
+    }
+}
+
+#[derive(Debug, PartialEq, Eq)]
+struct ParseStrategyError;
+impl fmt::Display for ParseStrategyError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "Invalid strategy")
+    }
+}
+
+#[derive(Debug, PartialEq, Eq)]
+struct FileTooShortError;
+impl fmt::Display for FileTooShortError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "Missing parameters.")
+    }
+}
+
+impl Error for FileTooShortError {}
+
+#[derive(Clone, Debug)]
 struct Cache {
     addr_size: u64,
     block_size: u64,
@@ -15,7 +46,7 @@ struct Cache {
     strat: Option<Strategy>,
 }
 
-#[derive(Clone)]
+#[derive(Clone, Debug)]
 struct CacheEntry {
     tag: u64,
     last_used: u64,
@@ -32,56 +63,63 @@ impl Cache {
     }
 
     fn idx_bits(&self) -> u64 {
-        (u64::BITS - self.assoc.leading_zeros()).into()
+        // log2(n_sets) = 
+        (u64::BITS - self.n_sets().leading_zeros() - 1).into()
     }
 }
 
-fn format_cache_line(line: &Vec<CacheEntry>, n: u64) -> String {
+fn format_cache_line(line: &[CacheEntry], n: u64) -> String {
     format!("{} | {:?}", n, line.last().map(|x| x.tag))
 }
 
-fn main() -> Result<(), Error> {
+fn main() -> Result<(), Box<dyn Error>> {
     let args: Vec<String> = env::args().collect();
-    println!("{}", args[1]);
+
     let (cache, addrs) = read(&args[1])?;
+
     let result = simulate(&cache, &addrs);
+
     for (i, line) in result.iter().enumerate() {
-        println!("{}", format_cache_line(line, i.try_into().unwrap()));
+        println!("{}", format_cache_line(line, (i as u64 / cache.n_sets()).try_into().unwrap()));
     }
-    println!("Hello, world!");
-    println!("{}", addrs[0]);
+
     Ok(())
 }
 
-fn read(path: &str) -> Result<(Cache, Vec<u64>), Error> {
+fn read(path: &str) -> Result<(Cache, Vec<u64>), Box<dyn Error>> {
     let content = fs::read_to_string(path).unwrap();
     let mut lines = content.lines();
-    let size = lines
-        .next()
-        .ok_or(Error::new(ErrorKind::Other, "Input too short."))?;
-    let addr_size: u64 = size.parse().unwrap();
-    let block_size = lines.next().unwrap();
-    let block_size: u64 = block_size.parse().unwrap();
-    let n_blocks = lines.next().unwrap();
-    let n_blocks: u64 = n_blocks.parse().unwrap();
-    let assoc = lines.next().unwrap();
-    let assoc: u64 = assoc.parse().unwrap();
+    
+    let mut int_parameters = lines.by_ref()
+        .take(4)
+        .map(|x| x.parse::<u64>());
+
+    let addr_size = int_parameters.next().ok_or(FileTooShortError)??;
+    let block_size: u64 = int_parameters.next().ok_or(FileTooShortError)??;
+    let n_blocks: u64 = int_parameters.next().ok_or(FileTooShortError)??;
+    let assoc = int_parameters.next().ok_or(FileTooShortError)??;
+    let strat = lines.next().ok_or(FileTooShortError)?.parse().ok();
+
     let addrs: Vec<u64> = lines
         .filter_map(|x| u64::from_str_radix(x, 16).ok())
         .collect();
+
     Ok((
         Cache {
             addr_size,
             block_size,
             n_blocks,
             assoc,
-            strat: None,
+            strat,
         },
         addrs,
     ))
 }
 
 fn simulate(cache: &Cache, addrs: &Vec<u64>) -> Vec<Vec<CacheEntry>> {
+    // result is a vector of cache lines. Each cache line is represented by a vector
+    // that is pushed to after every step since we don't only want to know the final state
+    // but also the state at each step.
     let mut result: Vec<Vec<CacheEntry>> = vec![
         vec![];
         cache.n_blocks.try_into().expect(
